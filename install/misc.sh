590a6c3170a44cb10b519150e291594fccbb5754
Nick McNutt <nmcnutt@utk.edu>
2017-08-26 10:44:03 -0400

    Add necessary packages for X windows

--- install/misc.sh
+++ install/misc.sh
@@ -1,7 +1,14 @@
 apt-get install -y \
     build-essential \
+    bzip2 \
+    ca-certificates \
     file \
+    git \
     gosu \
+    libglib2.0-0 \
+    libsm6 \
+    libxext6 \
+    libxrender1 \
     net-tools \
     sudo \
     tree \
